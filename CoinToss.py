9a53c57a8545918a33d4683adc66084abeb4d474
Oozegy <94505484+Oozegy@users.noreply.github.com>
2022-02-15 19:44:54 -0800

    This is the coin toss game pls like and subscribe

--- CoinToss.py
+++ CoinToss.py
@@ -1,6 +1,30 @@
-import random
+from random import randrange
+repeat = 'Yes'
+while repeat == 'Yes':
+    #Betting
+    coin_list = ['Heads', 'Tails']
+    user_bet = input('Hey user, do you want to choose head or the tails ')
+    computer_bet = 0
+    if user_bet == 'Heads':
+        computer_bet = 'Tails'
+    else:
+        computer_bet = 'Heads'
+    #Tossing
+    i = randrange(len(coin_list))
+    item = coin_list[i]
+    # Select item using index number
+    print("The coin landed on", item)
 
-sample_set = {"heads, tails"}
-item = random.choice(tuple(sample_set))
+    if item == user_bet:
+        print('You won!! good job buddy now get a life')
+    else:
+        print('The computer won :(')
 
-print(item)
+    repeat = input('Do you want to play againn? Say Yes or No')
+
+
+
+
+
+
+
